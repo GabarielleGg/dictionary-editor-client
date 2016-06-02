fd5eb06d1e58254a0093dc657b54384fd00f940b
hadware <hadwarez@gmail.com>
2016-06-01 20:41:55 -0400

    Code cleanup (removes useless commented code, removed unused handlers for annotations)

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var jwt    = require('jsonwebtoken'); 
+var jwt    = require('jsonwebtoken');
 var User   = require('./models/users');
 var mongoose    = require('mongoose');
 
@@ -15,14 +15,14 @@ var db_name='db3';
 var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name+'?authSource=admin';
+    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name+'?authSource=admin';
 }
 
 var db_name_users='dbusers';
 var mongodb_connection_string_users = 'mongodb://127.0.0.1:27017/' + db_name_users;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string_users = process.env.OPENSHIFT_MONGODB_DB_URL + db_name_users+'?authSource=admin';
+    mongodb_connection_string_users = process.env.OPENSHIFT_MONGODB_DB_URL + db_name_users+'?authSource=admin';
 }
 
 var db = require('mongoskin').db(mongodb_connection_string);
@@ -56,8 +56,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use(function (req, res, next) {
-  console.log('Time:', Date.now());
-  next();
+    console.log('Time:', Date.now());
+    next();
 });
 
 app.use(function(req,res,next){
@@ -66,64 +66,42 @@ app.use(function(req,res,next){
 });
 
 
-/*
-app.get('/setup', function(req, res) {
-
-  // create a sample user
-  var nick = new User({ 
-    name: 'max', 
-    password: 'password',
-    admin: true 
-  });
-
-  // save the sample user
-  nick.save(function(err) {
-    if (err) throw err;
-
-    console.log('User saved successfully');
-    res.json({ success: true });
-  });
-});
-
-*/
-
-
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 app.post('/authenticate', function(req, res) {
 
-  // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
+    // find the user
+    User.findOne({
+        name: req.body.name
+    }, function(err, user) {
 
-    if (err) throw err;
+        if (err) throw err;
 
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User not found.' });
+        } else if (user) {
 
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
+            // check if password matches
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+            } else {
 
-        // if user is found and password is right
-        // create a token
-        var token = jwt.sign(user, app.get('superSecret'), {
-          expiresIn: "24h" // expires in 24 hours
-        });
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign(user, app.get('superSecret'), {
+                    expiresIn: "24h" // expires in 24 hours
+                });
 
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
 
-    }
+        }
 
-  });
+    });
 });
 
 
@@ -147,20 +125,20 @@ app.use(function(req, res, next) {
 
             console.log("token value:"+token);
             delete req.body.token;
-    
+
             // verifies secret and checks exp
-            jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+            jwt.verify(token, app.get('superSecret'), function(err, decoded) {
                 if (err) {
                     console.log('token invalid!');
-                    return res.status(403).send({ 
-                        success: false, 
-                        message: 'Authentication required.' 
+                    return res.status(403).send({
+                        success: false,
+                        message: 'Authentication required.'
                     });
                     //return res.json({ success: false, message: 'Authentication required.' });     //or bad token message
                 } else {
                     // if everything is good, save to request for use in other routes
                     console.log("decoded value:"+decoded);
-                    req.decoded = decoded;    
+                    req.decoded = decoded;
                     next();
                 }
             });
@@ -169,13 +147,13 @@ app.use(function(req, res, next) {
 
             // if there is no token
             // return an error
-            return res.status(403).send({ 
-                success: false, 
-                message: 'Authentication required.' 
-            });   
+            return res.status(403).send({
+                success: false,
+                message: 'Authentication required.'
+            });
         }
     } else {
-        console.log("unsecured resource");   
+        console.log("unsecured resource");
         next();
     }
 });
@@ -183,9 +161,6 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 app.all('/api/allieml', restapi.allieml);
-app.all('/api/getannotations', restapi.getannotations);
-app.all('/api/removeannotation', restapi.removeannotation);
-app.all('/api/addannotation', restapi.addannotation);
 app.all('/api/getRelVisibility', restapi.getRelVisibility);
 app.all('/api/addRelVisibility', restapi.addRelVisibility);
 app.all('/api/remRelVisibility', restapi.remRelVisibility);
@@ -204,44 +179,25 @@ app.get('/templates/:name', restapi.templates);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-//if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
-      message: err.message,
-      error: err
+        message: err.message,
+        error: err
     });
-  });
-//}
+});
 
-/*/ production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});*/
 
 module.exports = app;
 
-/*var ipaddr = process.env.OPENSHIFT_INTERNAL_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
-app.listen(port, ipaddr);*/
-
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
+
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " +server_port )
+    console.log( "Listening on " + server_ip_address + ", server_port " +server_port )
 });
--- rels.js
+++ rels.js
@@ -7,7 +7,7 @@ var db_name='db3';
 var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name+'?authSource=admin';
+    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name+'?authSource=admin';
 }
 
 var db = require('mongoskin').db(mongodb_connection_string);
@@ -28,40 +28,34 @@ var main = function () {
     console.log("starting load...");
 
     //load terms into local array
-     db.collection('terms').find(/*{}, {IEML:1}*/).toArray(function(err, result) {
+    db.collection('terms').find(/*{}, {IEML:1}*/).toArray(function(err, result) {
         if (err) {
             console.log("ERROR"+err);
             throw err;
         }
-        
+
         console.log("loading terms "+result.length);
         var j = 0;
         for (var i=0;i<result.length;i++) {
-            
+
             allTerms[i] = result[i].IEML;
-            
-            //if (result[i].PARADIGM == "1") {
-                //console.log(JSON.stringify(result[i]));
-                //console.log(result[i].IEML);
-                //cursor[j++] = result[i].IEML;
-                cursor[j++] = result[i];
-            //}  
+
+            cursor[j++] = result[i];
         }
-        //console.log("found paradigms "+cursor.length);
 
         onIEMLLoaded();
     });
 };
 
 var onIEMLLoaded = function () {
-        db.collection('relationships').remove({}, function(err, result) {
+    db.collection('relationships').remove({}, function(err, result) {
         console.log('Emptied collection relationships');
-            onIEMLLoaded2();
-        });
+        onIEMLLoaded2();
+    });
 };
 
 var onIEMLLoaded2 = function() {
-    
+
     async.forEachLimit(cursor, 1, function(record, callbackMain) {
 
         currentIndx++;
@@ -69,112 +63,108 @@ var onIEMLLoaded2 = function() {
         var parseResult;
         async.series([
 
-            function (callback){
-                var http = require('http');
-                var querystring = require('querystring');
-                    
-                var isP = 0;
-                if (record.PARADIGM == "1")
-                    isP = 1;          
-                
-                var postData = querystring.stringify({
-                    'iemltext' : record.IEML,
-                    'parad'    : isP
-                });
-                
-                //console.log(postData);
+                function (callback){
+                    var http = require('http');
+                    var querystring = require('querystring');
 
-                var options = {
-                    hostname: 'test-ieml.rhcloud.com',
-                    port: 80,
-                    //hostname:'localhost',
-                    //port:8081,
-                    path: '/ScriptParser/rest/iemlparser/relationship2',
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Content-Length': postData.length
-                    }
-                };
+                    var isP = 0;
+                    if (record.PARADIGM == "1")
+                        isP = 1;
 
-                var body = '';
-                var req = http.request(options, function(res) {
-                    //console.log('STATUS: ' + res.statusCode);
-                    //console.log('HEADERS: ' + JSON.stringify(res.headers));
-                    res.setEncoding('utf8');
-                    res.on('data', function (chunk) {
-                        body += chunk;
+                    var postData = querystring.stringify({
+                        'iemltext' : record.IEML,
+                        'parad'    : isP
                     });
-                    res.on('end', function() {
-                    
-                        try {                       
-                            parseResult = JSON.parse(body);
-                            callback();
 
-                        } catch (e) {
-                            console.log("ERROR: problem parsing "+record.IEML);
-                            console.log("ERROR response"+body);
-                            countBad++;
-                            callback(new Error());
+                    var options = {
+                        hostname: 'test-ieml.rhcloud.com',
+                        port: 80,
+                        path: '/ScriptParser/rest/iemlparser/relationship2',
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Content-Length': postData.length
                         }
-                    })
-                });
+                    };
 
-                req.on('error', function(e) {
-                  console.log('problem with request: ' + e.message);
-                });
+                    var body = '';
+                    var req = http.request(options, function(res) {
+                        //console.log('STATUS: ' + res.statusCode);
+                        //console.log('HEADERS: ' + JSON.stringify(res.headers));
+                        res.setEncoding('utf8');
+                        res.on('data', function (chunk) {
+                            body += chunk;
+                        });
+                        res.on('end', function() {
 
-                // write data to request body
-                req.write(postData);
-                req.end();
-            },
-            function (callback) {
+                            try {
+                                parseResult = JSON.parse(body);
+                                callback();
 
-                //console.dir(parseResult);
-                var new_records = [];
+                            } catch (e) {
+                                console.log("ERROR: problem parsing "+record.IEML);
+                                console.log("ERROR response"+body);
+                                countBad++;
+                                callback(new Error());
+                            }
+                        })
+                    });
 
-                if (parseResult.relations.length==0) {
-                    console.log("EMPTY relations for "+record.IEML);
-                    countBad++;
-                    callback();
-                    return;
-                }
+                    req.on('error', function(e) {
+                        console.log('problem with request: ' + e.message);
+                    });
 
-                for (var i=0;i<parseResult.relations.length;i++) {
-                    var new_rec = {};
-                    new_rec.start = parseResult.relations[i].start;
-                    new_rec.ieml = parseResult.relations[i].stop;
-                    new_rec.visible = true;
-                    new_rec.exists = true;
-                    new_rec.type = parseResult.relations[i].name;
-                    if (allTerms.indexOf(new_rec.start)==-1 || allTerms.indexOf(new_rec.ieml)==-1 ) {
-                        new_rec.exists = false;
-                    }
-                    new_records.push(new_rec);
-                }
+                    // write data to request body
+                    req.write(postData);
+                    req.end();
+                },
+                function (callback) {
 
-                db.collection('relationships').insert(new_records, function(err, result) {
-                    if (err) {
-                        console .log("Error inserting into rels DB");
-                        console.dir(new_records);
-                        callback(err);
+                    //console.dir(parseResult);
+                    var new_records = [];
+
+                    if (parseResult.relations.length==0) {
+                        console.log("EMPTY relations for "+record.IEML);
+                        countBad++;
+                        callback();
                         return;
                     }
-                    countGood++;
-                    
-                    callback();
-                });
-            }
-        ], 
 
-        function(err) {
-            if (err) {
-                //console.log("ERROR>>>"+err);
-                callbackMain();
-                return;
-            }
-            callbackMain ();
-         });
+                    for (var i=0;i<parseResult.relations.length;i++) {
+                        var new_rec = {};
+                        new_rec.start = parseResult.relations[i].start;
+                        new_rec.ieml = parseResult.relations[i].stop;
+                        new_rec.visible = true;
+                        new_rec.exists = true;
+                        new_rec.type = parseResult.relations[i].name;
+                        if (allTerms.indexOf(new_rec.start)==-1 || allTerms.indexOf(new_rec.ieml)==-1 ) {
+                            new_rec.exists = false;
+                        }
+                        new_records.push(new_rec);
+                    }
+
+                    db.collection('relationships').insert(new_records, function(err, result) {
+                        if (err) {
+                            console .log("Error inserting into rels DB");
+                            console.dir(new_records);
+                            callback(err);
+                            return;
+                        }
+                        countGood++;
+
+                        callback();
+                    });
+                }
+            ],
+
+            function(err) {
+                if (err) {
+                    //console.log("ERROR>>>"+err);
+                    callbackMain();
+                    return;
+                }
+                callbackMain ();
+            });
 
     },  function(err) {
         console.log("Completed loading rels for "+countGood+" terms. "+countBad+" did not have any rels. Total processed "
