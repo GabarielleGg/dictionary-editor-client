9ec2efe1523cff8d0bb4bb564ed7c8de18d90df0
Candide Kemmler <ckemmler@gmail.com>
2016-06-08 14:37:37 +0200

    don't specify the server's ip address
    ...as it may be causing openshift to fail at running this app

--- app.js
+++ app.js
@@ -240,8 +240,8 @@ var port = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
 app.listen(port, ipaddr);*/
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " +server_port )
+app.listen(server_port, function () {
+  console.log( "Listening on server_port " +server_port )
 });
