e3a09d9896c7759ba712c5b5ab2c5ed4e2fa72ef
Louis van Beurden <louis.vanbeurden@gmail.com>
2017-11-23 15:10:35 -0500

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,3 +1,3 @@
-var api_host = 'api.ieml.io:5000';
-var api_basename = 'dictionary'
+var api_host = 'dictionary.ieml.io';
+var api_basename = 'api'
 var api_url = 'https://' + api_host + '/' + api_basename;
